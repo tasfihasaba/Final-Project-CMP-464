be0efb737f308ac1bbf832c5539ca2084247c404
Tasfiha Saba <77076887+tasfihasaba@users.noreply.github.com>
2023-05-23 20:25:21 -0400

    Add file

--- api/server.js
+++ api/server.js
@@ -11,6 +11,13 @@ mongoose.connect("mongodb://127.0.0.1:27017/mern-todo", {
     useUnifiedTopology: true
 }).then(() => console.log("Connected to DB")).catch(console.error);
 
+const Todo = require('./models/Todo');
+
+app.get('/todos', async (req, res) => {
+    const todos = await Todo.find();
+    res.json(todos);
+
+});
 app.listen(3001, () => console.log("Server started on port 3001"));
 
 
